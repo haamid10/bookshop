51f1bc6515635c80d7a8f915ee5d0efdeb21364d
haamid10 <apdixamiid919@gmail.com>
2022-07-17 20:10:57 +0300

    index3

--- admin/user/index.php
+++ admin/user/index.php
@@ -31,148 +31,25 @@ foreach($user->fetch_array() as $k =>$v){
 	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
 </script>
 <?php endif;?>
-<main id="main" class="main ml-0 bg-blue-50">
+
+<main id="main" class="main bg-blue-100 rounded-xl">
 <div class="pagetitle">
       <h1>My profile</h1>
      
 </div>
 <section class="section profile">
   <div class="row">
-   	<div class="col-xl-4">
+	<div class="col-xl-4">
 		
-		 <!-- profileka-hoose -->
-	   	<div class="card mr-96">
-            <div class=" card-body profile-card pt-6 d-flex flex-column align-items-center">
-						<span><img src="<?php echo validate_image($_settings->userdata('avatar')) ?>" alt="user image" alt="Profile" class="rounded-circle"></span>
-					
-						<h2 class="d-none d-md-block  ps-2"><?php echo ucwords($_settings->userdata('firstname').' '.$_settings->userdata('lastname')) ?></h2>
-						
-						
-              <div class="social-links mt-2">
-                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
+		<!-- profileka-hoose -->
 
-	</div>
+<section class="section-profile">
+<div class="col-xl-8 ">
 
-					<div class="col-xl-8">
-
-  <div class="card">
-    	<div class="card-body pt-2">
 		
-	
-			<ul class="nav nav-tabs nav-tabs-bordered">
-        <li class="nav-item">
-	        <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#profile-overview">Overview</button>
-          </li>
 
-          <li class="nav-item">
-	          <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-edit">Edit Profile</button>
-                </li>
-
-              <li class="nav-item">
-	             <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-settings">Settings</button>
-                   </li>
-
-                 <li class="nav-item">
-	                  <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-change-password">Change Password</button>
-                     </li>
-	    </ul>
-			</div>
-
-	 <div class="tab-content pt-2">
-
-     <div class="tab-pane w-fit fade show active profile-overview" id="profile-overview ">
-
-		 
-                  <h5 class="card-title">About</h5>
-                 
-                  <h5 class="card-title">Profile Details</h5>
-
-                  <div class="row">
-                    <div class="col-lg-3 col-md-4 label ">Full Name</div>
-                    <div class="col-lg-9 col-md-8">
-									<?php echo ucwords($_settings->userdata('firstname').' '.$_settings->userdata('lastname')) ?>
-                  </div>
-                     </div>
-
-                  
-
-                  <div class="row">
-                    <div class="col-lg-3 col-md-4 label">role</div>
-                    <div class="col-lg-9 col-md-8">Administrator</div>
-                  </div>
-
-                  <div class="row">
-                    <div class="col-lg-3 col-md-4 label">Country</div>
-                    <div class="col-lg-9 col-md-8">Somaliland</div>
-               </div>
-									
-     </div>	
-	
-		 <div class="tab-content pt-2">
-
-                <div class="tab-pane fade show active profile-overview" id="profile-overview">
-                  <h5 class="card-title">About</h5>
-                  <p class="small fst-italic">Sunt est soluta temporibus accusantium neque nam maiores cumque temporibus. Tempora libero non est unde veniam est qui dolor. Ut sunt iure rerum quae quisquam autem eveniet perspiciatis odit. Fuga sequi sed ea saepe at unde.</p>
-
-                  <h5 class="card-title">Profile Details</h5>
-
-                  <div class="row">
-                    <div class="col-lg-3 col-md-4 label ">Full Name</div>
-                    <div class="col-lg-9 col-md-8">Kevin Anderson</div>
-                  </div>
-
-                  <div class="row">
-                    <div class="col-lg-3 col-md-4 label">Company</div>
-                    <div class="col-lg-9 col-md-8">Lueilwitz, Wisoky and Leuschke</div>
-                  </div>
-
-                  <div class="row">
-                    <div class="col-lg-3 col-md-4 label">Job</div>
-                    <div class="col-lg-9 col-md-8">Web Designer</div>
-                  </div>
-
-
-                </div>
-		
-	<!-- qaybta editka -->
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- before -->
-	 </div>
-	 </div>
-<!--  qaybta borderka-->
- </div>
-</section>
-
-
-<div class="card card-outline card-primary">
+<div class="tab-pane " >
+					<div class="card card" >
 	<div class="card-body">
 		<div class="container-fluid">
 			<div id="msg"></div>
@@ -215,7 +92,41 @@ foreach($user->fetch_array() as $k =>$v){
 				</div>
 			</div>
 		</div>
+					</div>
 </div>
+</div>
+
+
+
+
+ </div>
+</section>
+
+<section class=" ml-96 mt-2" id="section-profile">
+	<div class="row ">
+<div class="col-xl-4">
+		
+		<!-- profileka-hoose -->
+ <div class="card rounded ">
+					 <div class=" card-body profile-card pt-6 d-flex flex-column align-items-center" id="manage-user">
+				 
+				 
+					 <h2 class="d-none d-md-block  ps-2"><?php echo ucwords($_settings->userdata('firstname').' '.$_settings->userdata('lastname')) ?></h2>
+					 
+					 
+						 <div class="social-links mt-2">
+							 <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+							 <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+							 <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+							 <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+						 </div>
+					 </div>
+ </div>
+</div>
+
+	
+</section>
+</main>
 
 <style>
 	img#cimg{
@@ -259,18 +170,3 @@ foreach($user->fetch_array() as $k =>$v){
 	})
 
 </script>
-
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-<!-- Vendor JS Files -->
-<script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/vendor/chart.js/chart.min.js"></script>
-<script src="assets/vendor/echarts/echarts.min.js"></script>
-<script src="assets/vendor/quill/quill.min.js"></script>
-<script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
-<script src="assets/vendor/tinymce/tinymce.min.js"></script>
-<script src="assets/vendor/php-email-form/validate.js"></script>
-
-<!-- Template Main JS File -->
-<script src="assets/js/main.js"></script>
