36983429c7f7986b44771a0ba2d9f078541836b1
haamid10 <apdixamiid919@gmail.com>
2022-07-17 10:58:20 +0300

    index12

--- admin/user/index.php
+++ admin/user/index.php
@@ -31,17 +31,18 @@ foreach($user->fetch_array() as $k =>$v){
 	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
 </script>
 <?php endif;?>
-<main id="main" class="main bg-blue-50">
+<main id="main" class="main ml-0 bg-blue-50">
 <div class="pagetitle">
       <h1>My profile</h1>
      
-    </div
+</div>
 <section class="section profile">
-<div class="row">
-	<div class="col-xl-6">
+  <div class="row">
+   	<div class="col-xl-4">
 		
-		<div class="card">
-            <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
+		 <!-- profileka-hoose -->
+	   	<div class="card mr-96">
+            <div class=" card-body profile-card pt-6 d-flex flex-column align-items-center">
 						<span><img src="<?php echo validate_image($_settings->userdata('avatar')) ?>" alt="user image" alt="Profile" class="rounded-circle"></span>
 					
 						<h2 class="d-none d-md-block  ps-2"><?php echo ucwords($_settings->userdata('firstname').' '.$_settings->userdata('lastname')) ?></h2>
@@ -60,28 +61,30 @@ foreach($user->fetch_array() as $k =>$v){
 
 					<div class="col-xl-8">
 
-<div class="card">
-	<div class="card-body pt-3">
+  <div class="card">
+    	<div class="card-body pt-2">
 		
-	<ul class="nav nav-tabs nav-tabs-bordered">
+	
+			<ul class="nav nav-tabs nav-tabs-bordered">
+        <li class="nav-item">
+	        <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#profile-overview">Overview</button>
+          </li>
 
-<li class="nav-item">
-	<button class="nav-link active" data-bs-toggle="tab" data-bs-target="#profile-overview">Overview</button>
-</li>
+          <li class="nav-item">
+	          <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-edit">Edit Profile</button>
+                </li>
 
-<li class="nav-item">
-	<button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-edit">Edit Profile</button>
-</li>
+              <li class="nav-item">
+	             <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-settings">Settings</button>
+                   </li>
 
-<li class="nav-item">
-	<button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-settings">Settings</button>
-</li>
+                 <li class="nav-item">
+	                  <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-change-password">Change Password</button>
+                     </li>
+	    </ul>
+			</div>
 
-<li class="nav-item">
-	<button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-change-password">Change Password</button>
-</li>
-	</ul>
-	<div class="tab-content pt-2">
+	 <div class="tab-content pt-2">
 
      <div class="tab-pane w-fit fade show active profile-overview" id="profile-overview ">
 
@@ -95,6 +98,7 @@ foreach($user->fetch_array() as $k =>$v){
                     <div class="col-lg-9 col-md-8">
 									<?php echo ucwords($_settings->userdata('firstname').' '.$_settings->userdata('lastname')) ?>
                   </div>
+                     </div>
 
                   
 
@@ -107,108 +111,72 @@ foreach($user->fetch_array() as $k =>$v){
                     <div class="col-lg-3 col-md-4 label">Country</div>
                     <div class="col-lg-9 col-md-8">Somaliland</div>
                </div>
-									</div>
-</div>	
+									
+     </div>	
+	
+		 <div class="tab-content pt-2">
+
+                <div class="tab-pane fade show active profile-overview" id="profile-overview">
+                  <h5 class="card-title">About</h5>
+                  <p class="small fst-italic">Sunt est soluta temporibus accusantium neque nam maiores cumque temporibus. Tempora libero non est unde veniam est qui dolor. Ut sunt iure rerum quae quisquam autem eveniet perspiciatis odit. Fuga sequi sed ea saepe at unde.</p>
+
+                  <h5 class="card-title">Profile Details</h5>
+
+                  <div class="row">
+                    <div class="col-lg-3 col-md-4 label ">Full Name</div>
+                    <div class="col-lg-9 col-md-8">Kevin Anderson</div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-lg-3 col-md-4 label">Company</div>
+                    <div class="col-lg-9 col-md-8">Lueilwitz, Wisoky and Leuschke</div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-lg-3 col-md-4 label">Job</div>
+                    <div class="col-lg-9 col-md-8">Web Designer</div>
+                  </div>
+
+
+                </div>
 		
-		
-		
-		
-<div class="tab-pane fade profile-edit pt-3" id="profile-edit">
-<form>
-<div class="form-group">
-			
-                    <div class="row mb-3">
-                      <label for="profileImage" class="col-md-4 col-lg-3 col-form-label">Profile Image</label>
-                      <div class="col-md-8 col-lg-9">
-											<label class="btn btn-primary" for="customFile">Choose file</label>
-											<input type="file" class="btn btn-primary" onchange="displayImg(this,$(this))"> 
-											
-                          
-                         
-													<img src="<?php echo validate_image(isset($meta['avatar']) ? $meta['avatar'] :'') ?>" alt=""  >
-                        
-                      </div>
-                    </div>
-           	<input type="hidden" name="id" value="<?php echo $_settings->userdata('id') ?>">
-			
-                    <div class="row mb-3">
-										<label class="col-md-4 col-lg-3 col-form-label" for="name">First Name</label>
-                      
-                      <div class="col-md-8 col-lg-9">
-                        
-											
-					<input type="text" name="firstname" id="firstname" class="form-control" value="<?php echo isset($meta['firstname']) ? $meta['firstname']: '' ?>" required>
-                      </div>
-                    </div>
+	<!-- qaybta editka -->
 
-                   
-                    <div class="row mb-3">
-                      <label for="Phone" class="col-md-4 col-lg-3 col-form-label">Phone</label>
-                      <div class="col-md-8 col-lg-9">
-                        <input name="phone" type="text" class="form-control" id="Phone" value="(436) 486-3538 x29071">
-                      </div>
-                    </div>
-
-                    
-                    <div class="row mb-3">
-                      <label for="Twitter" class="col-md-4 col-lg-3 col-form-label">T</label>
-                      <div class="col-md-8 col-lg-9">
-                        <input name="twitter" type="text" class="form-control" id="Twitter" value="https://twitter.com/#">
-                      </div>
-                    </div>
-
-                   
-                    <div class="text-center">
-                      
-											<button class="btn btn-primary"  form="manage-user">
-                      save changes
-                    </button>
-                    </div>
-										
-					
-				</div>
-                  </form><!-- End Profile Edit Form -->
-
-									<div class="form-group">
-					<label for="name">Last Name</label>
-					<input type="text" name="lastname" id="lastname" class="form-control" value="<?php echo isset($meta['lastname']) ? $meta['lastname']: '' ?>" required>
-				</div>
-				<div class="form-group">
-					<label for="username">Username</label>
-					<input type="text" name="username" id="username" class="form-control" value="<?php echo isset($meta['username']) ? $meta['username']: '' ?>" required  autocomplete="off">
-				</div>
-				<div class="form-group">
-					<label for="password">Password</label>
-					<input type="password" name="password" id="password" class="form-control" value="" autocomplete="off">
-					<small><i>Leave this blank if you dont want to change the password.</i></small>
-				</div>
-				<div class="form-group">
-					<label for="" class="control-label">Avatar</label>
-					<div class="custom-file">
-		              <input type="file" class="custom-file-input rounded-circle" id="customFile" name="img" onchange="displayImg(this,$(this))">
-		              <label class="custom-file-label" for="customFile">Choose file</label>
-		            </div>
-				</div>
-				<div class="form-group d-flex justify-content-center">
-					<img src="<?php echo validate_image(isset($meta['avatar']) ? $meta['avatar'] :'') ?>" alt="" id="cimg" class="img-fluid img-thumbnail">
-				</div>
-			</form>
-		</div>
-	</div>
+	
 
 
-                  
-				<button class="accordion-button w-28"  form="manage-user">
-                      Accordion Item #1
-                    </button>
-					
-				</div>
-			</div>
 
 
-</div>		
-		
-								<form action="" id="manage-user">	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- before -->
+	 </div>
+	 </div>
+<!--  qaybta borderka-->
+ </div>
+</section>
+
+
+<div class="card card-outline card-primary">
+	<div class="card-body">
+		<div class="container-fluid">
+			<div id="msg"></div>
+			<form action="" id="manage-user">	
 				<input type="hidden" name="id" value="<?php echo $_settings->userdata('id') ?>">
 				<div class="form-group">
 					<label for="name">First Name</label>
@@ -240,18 +208,15 @@ foreach($user->fetch_array() as $k =>$v){
 			</form>
 		</div>
 	</div>
-
-
-                  
-				<button class="accordion-button w-28"  form="manage-user">
-                      Accordion Item #1
-                    </button>
-					
+	<div class="card-footer">
+			<div class="col-md-12">
+				<div class="row">
+					<button class="btn btn-sm btn-primary" form="manage-user">Update</button>
 				</div>
 			</div>
 		</div>
 </div>
-</section>
+
 <style>
 	img#cimg{
 		height: 15vh;
@@ -294,3 +259,18 @@ foreach($user->fetch_array() as $k =>$v){
 	})
 
 </script>
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/chart.js/chart.min.js"></script>
+<script src="assets/vendor/echarts/echarts.min.js"></script>
+<script src="assets/vendor/quill/quill.min.js"></script>
+<script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="assets/vendor/tinymce/tinymce.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
